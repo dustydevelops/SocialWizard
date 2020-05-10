d0f8b63e2e98d52f94c9249d9242279ac5f33270
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2020-05-10 15:05:02 -0600

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,55 +1,49 @@
 
-################################################################################################################
+##############################################################################################
 #                                        SOCIAL WIZARD
-################################################################################################################
-
+##############################################################################################
 username = 'joeshmo'           # the username the bot will use to log in.
 password = 'youllneverguess'   # the password the bot will use to log in.
-
-################################################################################################################
-###############for  quick start just input your details above and then run. the rest of this doesnt matter######
-################################################################################################################
-
+###############################################################################################
+############## just input your details above and then run. the rest of this doesnt matter######
+###############################################################################################
+###############################################################################################
+###############################################################################################
+###############################################################################################
+################################# Import statements ###########################################
 from selenium import webdriver      # from selenium import webdriver for web browser automation.
 from time import  sleep             # import sleep from time for creating time delays.
-
+###############################################################################################
+################################ URL Variables ################################################
+###############################################################################################
 url = 'http://www.instagram.com/'   #I nstagrams url
 loggedIn = url + 'accounts/login/'  # Instagrams login url
-profile = url + username.           # Your profile
+profile = url + username          # Your profile
+#######################################################################
+################# Start instagram login process #######################
+#######################################################################
 
-################################################################################################################
-################# Start instagram login process if engage = True #######################
-################################################################################################################
-engage = True
-while engage == True :
+dr = webdriver.Safari() #   initialize the safari window
+dr.get(loggedIn)        #    Go to login page 
+sleep(3)                #    Give the page a chance to load
+                          
+element = dr.find_element_by_xpath("//input[@name='username']")
+element.send_keys(username) #    Enter username
+element = dr.find_element_by_xpath("//input[@name='password']")
+element.send_keys(password) #   Enter password 
 
-    #################################   initialize the safari window
-    dr = webdriver.Safari()
-    try:
-        ################################    Go to login page 
-        dr.get(loggedIn)
-        ################################    Give the page a chance to load
-        sleep(3)
-        #################################   Find the username textfield
-        element = dr.find_element_by_xpath("//input[@name='username']")
-        #################################    Enter username 
-        element.send_keys(username)
-        #################################   Find the password textfield
-        element = dr.find_element_by_xpath("//input[@name='password']")
-        #################################   Enter password 
-        element.send_keys(password)
-        #################################   Locate login button
-        login_button = dr.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')
-        ################################    Let the script breathe
-        sleep(3)
-        #################################   Click login button
-        login_button.click() and print ('Username and password input succesfully now logging in')
-        ################################    Let the script breathe
-        sleep(6)
+xPath1 = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button'
+login_button = dr.find_element_by_xpath(xPath1) 
+login_button.click() # click the login button
 
+#######################################################################
+#################        end instagram bot        #####################
+#######################################################################
+sleep(60)            #    Log out 60 seconds after the script is done
+dr.close()           #    close window
+#######################################################################
+#################     github.com/DustyDevelops    #####################
+#######################################################################
 
-
-    ################################    close window
-    dr.close()
 
 
