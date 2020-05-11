a7fc53b13e902a5f37c5efa64d27ca147e1025fe
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2020-05-11 16:23:26 -0600

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -1,34 +1,10 @@
 
-###############################################################################################
-#                                        SOCIAL WIZARD
-###############################################################################################
-email = 'joeshmo@gmail.com'
-phone = '() - '
-username = 'joshmo'           # the username the bot will use to log in.
-password = 'youllneverguesthis'   # the password the bot will use to log in.
-
-#                        SOCIAL WIZARD                          #
-
-from selenium import webdriver 
-from time import sleep
-
-#       User Credentials        #
-
-email = 'dustinmichaelsmith03@gmail.com'
-phone = '() - '
-username = 'dsmithnoswag'        
-password = '1dirtysock'   
-
-#            Urls               #
-
 #                Start Facebook Engagement                   #
 
 
 # Initialize the safari window with facebook login page & wait
 dr = webdriver.Safari()           
 dr.get('https://www.facebook.com/login')
-
-
 
 
 element = dr.find_elements_by_xpath('//*[@id ="email"]') 
@@ -45,30 +21,64 @@ login_button = dr.find_element_by_xpath('//*[@id="loginbutton"]')
 login_button.click() # click the login button
 sleep(3)
 print("Login Successfull")
-
+sleep(3)
 dr.get('https://www.facebook.com/events/birthdays')
+sleep(3)
+tellThem = 'Happy Birthday!'
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_12"]')
+        element.send_keys(tellThem)
+except:
+        pass
+element.submit()
+sleep(0.1)
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_13"]')
+        element.send_keys(tellThem)
+except:
+        pass
+element.submit()
+sleep(0.1)
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_16"]')
+        element.send_keys(tellThem)
+except:
+        pass
+element.submit()
+sleep(0.1)
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_17"]')
+        element.send_keys(tellThem)
+except:
+        pass
+element.submit()
+sleep(0.1)
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_1a"]')
+        element.send_keys(tellThem)
+except:
+        pass
+element.submit()
+sleep(0.1)
+try:
+        element = dr.find_element_by_xpath('//*[@id="u_0_1d"]')
+        element.send_keys(tellThem)
+except:
+        pass
 
-tellThem = 'Happy Birthday'
-if tellThem == 'Happy Birthday':
-        try:
-                element = dr.find_element_by_xpath('//*[@id="u_0_13"]')
-                element.send_keys(tellThem)
-                sleep(0.1)
-                print("Birthday Wish posted for friend 1") 
+element.submit()
+sleep(0.1)
+
+sleep(0.1)
+print("Birthday Wish posted for friend 1") 
               
-           
-        except:
-                pass
-                
+    
 
 #######################################################################
-#################        end facebook bot        #####################
-#######################################################################
-sleep(60)            #    Log out 60 seconds after the script is done
+
+sleep(30)            #    Log out 60 seconds after the script is done
 dr.close()           #    close window
-#######################################################################
-#################     github.com/DustyDevelops    #####################
-#######################################################################
 
+#################     github.com/DustyDevelops    #####################
 
 
