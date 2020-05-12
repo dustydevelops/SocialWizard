b355968d83e855c7662f40c2af5397362523c1b9
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2020-05-12 13:47:49 -0600

    added the ability to loop every 2 hours
    to check for new/missed birthdays
    
    also added the ability to ignore errors

--- facebook.py
+++ facebook.py
@@ -1,84 +1,111 @@
 
-#                Start Facebook Engagement                   #
+
+#                        SOCIAL WIZARD                          #
+
+from selenium import webdriver 
+import time
+import sys
 
 
-# Initialize the safari window with facebook login page & wait
-dr = webdriver.Safari()           
-dr.get('https://www.facebook.com/login')
 
+#       User Credentials        #
 
-element = dr.find_elements_by_xpath('//*[@id ="email"]') 
-element[0].send_keys(email) 
-
-print("Username Entered") 
-
-element = dr.find_element_by_xpath('//*[@id ="pass"]') 
-element.send_keys(password) 
-
-print("Password Entered") 
-
-login_button = dr.find_element_by_xpath('//*[@id="loginbutton"]') 
-login_button.click() # click the login button
-sleep(3)
-print("Login Successfull")
-sleep(3)
-dr.get('https://www.facebook.com/events/birthdays')
-sleep(3)
+email = 'darealjoeshmo@gmail.com'
+password = 'youllneverguessit'   
 tellThem = 'Happy Birthday!'
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_12"]')
-        element.send_keys(tellThem)
-except:
-        pass
-element.submit()
-sleep(0.1)
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_13"]')
-        element.send_keys(tellThem)
-except:
-        pass
-element.submit()
-sleep(0.1)
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_16"]')
-        element.send_keys(tellThem)
-except:
-        pass
-element.submit()
-sleep(0.1)
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_17"]')
-        element.send_keys(tellThem)
-except:
-        pass
-element.submit()
-sleep(0.1)
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_1a"]')
-        element.send_keys(tellThem)
-except:
-        pass
-element.submit()
-sleep(0.1)
-try:
-        element = dr.find_element_by_xpath('//*[@id="u_0_1d"]')
-        element.send_keys(tellThem)
-except:
-        pass
 
-element.submit()
-sleep(0.1)
-
-sleep(0.1)
-print("Birthday Wish posted for friend 1") 
-              
-    
-
-#######################################################################
-
-sleep(30)            #    Log out 60 seconds after the script is done
-dr.close()           #    close window
-
-#################     github.com/DustyDevelops    #####################
+#                Start Facebook Engagement                   #
+tellThem = 'HappyBirthday!'
+x = 1
+while True:
+        print('Iteration:', x)
+        dr = webdriver.Safari()           
+        dr.get('https://www.facebook.com/login')
 
 
+        element = dr.find_elements_by_xpath('//*[@id ="email"]') 
+        element[0].send_keys(email) 
+
+        print("Username Entered") 
+
+        element = dr.find_element_by_xpath('//*[@id ="pass"]') 
+        element.send_keys(password) 
+
+        print("Password Entered") 
+
+        login_button = dr.find_element_by_xpath('//*[@id="loginbutton"]') 
+        login_button.click() # click the login button
+
+        print("Login Successfull")
+        time.sleep(3)
+        dr.get('https://www.facebook.com/events/birthdays')
+        time.sleep(3)
+        
+        
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_12"]')
+                element.send_keys(tellThem)
+                sleep(1)
+                element.submit()
+                print("Birthday wish 1 Successfull")
+        except:
+                pass
+        
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_13"]')
+                element.send_keys(tellThem)
+                time.sleep(1)
+                element.submit()
+                print("a birthday wish was made")
+
+        except:
+                pass
+
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_16"]')
+                element.send_keys(tellThem)
+                time.sleep(1)
+                element.submit()
+                print("A birthday wish was made")
+        except:
+                pass
+     
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_17"]')
+                element.send_keys(tellThem)
+                time.sleep(1)
+                element.submit()
+                print("A birthday wish was made")
+        except:
+                pass
+
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_1a"]')
+                element.send_keys(tellThem)
+                time.sleep(1)
+                element.submit()
+                print("A birthday wish was made")
+                
+
+        except:
+                pass
+     
+        try:
+                element = dr.find_element_by_xpath('//*[@id="u_0_1d"]')
+                element.send_keys(tellThem)
+                time.sleep(1)
+                element.submit()
+                print("A birthday wish was made")
+
+        except:
+                pass
+        print('Birthday post process complete')
+        print('Iteration', x , 'Was completed successfully.')
+        print('In 2 hours the appication will recheck for new/missed birthdays.','\n',
+                'Please do not power down your system ')
+        
+
+
+        dr.close()
+
+        time.sleep(7200)
