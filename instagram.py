acb28f100a0fe580bb4d21deb4619eb0c074569c
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2021-11-09 15:16:07 -0800

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,27 +1,15 @@
 
-##############################################################################################
-#                                        SOCIAL WIZARD
-##############################################################################################
+
 username = 'joeshmo'           # the username the bot will use to log in.
 password = 'youllneverguess'   # the password the bot will use to log in.
-###############################################################################################
-############## just input your details above and then run. the rest of this doesnt matter######
-###############################################################################################
-###############################################################################################
-###############################################################################################
-###############################################################################################
-################################# Import statements ###########################################
+
 from selenium import webdriver      # from selenium import webdriver for web browser automation.
 from time import  sleep             # import sleep from time for creating time delays.
-###############################################################################################
-################################ URL Variables ################################################
-###############################################################################################
+
 url = 'http://www.instagram.com/'   #I nstagrams url
 loggedIn = url + 'accounts/login/'  # Instagrams login url
 profile = url + username          # Your profile
-#######################################################################
-################# Start instagram login process #######################
-#######################################################################
+
 
 dr = webdriver.Safari() #   initialize the safari window
 dr.get(loggedIn)        #    Go to login page 
@@ -36,14 +24,8 @@ xPath1 = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div
 login_button = dr.find_element_by_xpath(xPath1) 
 login_button.click() # click the login button
 
-#######################################################################
-#################        end instagram bot        #####################
-#######################################################################
 sleep(60)            #    Log out 60 seconds after the script is done
 dr.close()           #    close window
-#######################################################################
-#################     github.com/DustyDevelops    #####################
-#######################################################################
 
 
 
