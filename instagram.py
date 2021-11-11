34f745239e7f27f4ab8e2082fdf82529a2ab6d7a
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2021-11-10 19:59:20 -0800

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,31 +1,131 @@
 
+import random,time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-username = 'joeshmo'           # the username the bot will use to log in.
-password = 'youllneverguess'   # the password the bot will use to log in.
+# If you have multiple instagrams put multiple usernames
+usernames = ['or','you can delete','and add more here']
 
-from selenium import webdriver      # from selenium import webdriver for web browser automation.
-from time import  sleep             # import sleep from time for creating time delays.
+# passwords only seem to work if you input integers, special characters and letters in seperate
+#for examply if your pass word is 1FakePassword!, put it in as follows
+#see def inputThePassword() near line 47 to understand the logic on the following
+passwords = [1,'FakePassword','!','besuretoadd',2,'all combations','if you use multiple accts']
 
-url = 'http://www.instagram.com/'   #I nstagrams url
-loggedIn = url + 'accounts/login/'  # Instagrams login url
-profile = url + username          # Your profile
+# If you use Apples Safari no action. If you use chrome or firefox, edit the following line. 
+driver = webdriver.Safari()
 
-
-dr = webdriver.Safari() #   initialize the safari window
-dr.get(loggedIn)        #    Go to login page 
-sleep(3)                #    Give the page a chance to load
-                          
-element = dr.find_element_by_xpath("//input[@name='username']")
-element.send_keys(username) #    Enter username
-element = dr.find_element_by_xpath("//input[@name='password']")
-element.send_keys(password) #   Enter password 
-
-xPath1 = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button'
-login_button = dr.find_element_by_xpath(xPath1) 
-login_button.click() # click the login button
-
-sleep(60)            #    Log out 60 seconds after the script is done
-dr.close()           #    close window
+def humanizeIt():
+    godsWill = [float(random.randint(1,9)) in [3,6,9] , float(random.randint(1,5))]
+    return godsWill
 
 
+def initiateWebBrowser():
+    try:
+        
+        print('Setting the bounds of the safari window...')
+        driver.set_window_position(0, 0)
+        driver.set_window_size(1024, 768)
+        print('Retreiving instagram....')
+        driver.get('https://www.instagram.com/')
+        time.sleep(humanizeIt()[1])
+        return driver
+    except Exception as e:
+        print(e)
 
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+def inputTheUsername():
+    
+    driver.find_element_by_css_selector("input[name='username']").send_keys(usernames[1])
+    time.sleep(humanizeIt()[1])
+
+def inputThePassword():
+    
+    password = driver.find_element_by_css_selector("input[name='password']")
+    password.send_keys(passwords[0])
+    password.send_keys(passwords[2])
+    time.sleep(humanizeIt()[1])
+    password.send_keys(passwords[3])
+
+def showAndHidePassword():
+    if humanizeIt()[0] == True:
+        driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/div/div/button').click()
+        time.sleep(float(humanizeIt()[1]))
+        if humanizeIt()[0] == True:
+            driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/div/div/button').click()
+            time.sleep(humanizeIt()[1])
+
+
+
+
+def login():
+    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+    time.sleep(humanizeIt()[1])
+    xpath='//*[@id="slfErrorAlert"]'
+    print('FAILED TO LOGIN',check_exists_by_xpath(xpath))
+
+
+
+try:
+
+
+
+    driver = initiateWebBrowser()
+    inputTheUsername()
+    inputThePassword()
+    showAndHidePassword()
+    login()
+
+    #driver.close()
+except Exception as e:
+
+    print(e)
+'''           
+
+time.sleep(5)
+notNow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+time.sleep(3)
+article = 1
+
+while True:
+        post = str(article)
+        like = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div/div[2]/div/article['+post+']/div/div[3]/div/div/section[1]/span[1]/button')
+        like.click()
+        time.sleep((random.randint(0,5)))
+        print('post',post,"liked..")
+        article+=1
+        if article == 6:
+            break
+
+explore = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/a')
+explore.click()
+time.sleep(5)
+firstPicture = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/div/div[1]/div[2]/div')
+firstPicture.click()
+time.sleep(3.333)
+followFirstProfile = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]')
+followFirstProfile.click()
+visitFirstProfile = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[1]')
+visitFirstProfile.click()
+time.sleep(5)
+followers = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]')
+followers.click()
+time.sleep(5)
+
+driver.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/ul/div/li[2]/div/div[1]').click()
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button').click()
+time.sleep(3)
+explore = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/a')
+explore.click()
+time.sleep(3)
+secondPicture = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/div/div[1]/div[1]')
+secondPicture.click()
+time.sleep(20)
+driver.close()
+'''
